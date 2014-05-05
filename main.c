53a45736bb346485e21ce1cfa94ecaa18ab736c9
Markus Stenberg <markus.stenberg@iki.fi>
2014-05-05 17:00:39 +0200

    Added initial skeleton.

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * $Id: main.c $
+ *
+ * Author: Markus Stenberg <markus stenberg@iki.fi>
+ *
+ * Copyright (c) 2014 cisco Systems, Inc.
+ *
+ * Created:       Mon May  5 16:53:27 2014 mstenber
+ * Last modified: Mon May  5 16:53:37 2014 mstenber
+ * Edit time:     0 min
+ *
+ */
+
+int main(int argc, char **argv)
+{
+
+  return 0;
+}
